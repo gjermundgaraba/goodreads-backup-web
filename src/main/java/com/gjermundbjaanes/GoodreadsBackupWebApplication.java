01c918b98b12f8597db4ab64250bcc6e6ce2110e
Gjermund Bjaanes <bjaanes@gmail.com>
2016-11-28 20:50:22 +0100

    Add support for war deployment

--- src/main/java/com/gjermundbjaanes/GoodreadsBackupWebApplication.java
+++ src/main/java/com/gjermundbjaanes/GoodreadsBackupWebApplication.java
@@ -2,9 +2,16 @@ package com.gjermundbjaanes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GoodreadsBackupWebApplication {
+public class GoodreadsBackupWebApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GoodreadsBackupWebApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GoodreadsBackupWebApplication.class, args);
